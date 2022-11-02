2290999e4fbcc98b7e6f08dfdd4a4cf32eeee96e
tkt <37575408+tktcorporation@users.noreply.github.com>
2022-11-02 16:34:30 +0000

    :sparkles: update ToggleField

--- web/src/components/ToggleField/ToggleField.tsx
+++ web/src/components/ToggleField/ToggleField.tsx
@@ -4,12 +4,14 @@ interface ToggleFieldProps {
   label: string
   name: string
   checked: boolean
+  loading?: boolean
   onChange: (value: boolean) => void
 }
 const ToggleField = ({
   label,
   name,
   checked,
+  loading,
   onChange,
 }: ToggleFieldProps): JSX.Element => {
   return (
@@ -21,6 +23,7 @@ const ToggleField = ({
         checked={checked}
         onChange={onChange}
         name={name}
+        disabled={loading}
         className={`${
           checked ? 'bg-indigo-600' : 'bg-gray-200'
         } relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
