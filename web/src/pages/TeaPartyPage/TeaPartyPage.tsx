ee5ef42ab793023e57bb08d3de40ae112a1e79a2
tkt <37575408+tktcorporation@users.noreply.github.com>
2022-12-09 13:54:51 +0000

    wip

--- web/src/pages/TeaPartyPage/TeaPartyPage.tsx
+++ web/src/pages/TeaPartyPage/TeaPartyPage.tsx
@@ -1,8 +1,14 @@
 import { MetaTags } from '@redwoodjs/web'
 
-import NewTeaParty from 'src/components/TeaParty/NewTeaParty'
+// import NewTeaParty from 'src/components/TeaParty/NewTeaParty'
+import TeaPartyForm from 'src/components/TeaParty/TeaPartyForm'
 
 const TeaPartyPage = () => {
+  const onSave = (input) => {
+    console.log('onSave', input)
+  }
+  const error = null
+  const loading = false
   return (
     <>
       <MetaTags title="TeaParty" description="TeaParty page" />
@@ -10,7 +16,8 @@ const TeaPartyPage = () => {
       <div className="flex h-full w-full flex-col items-center justify-center">
         {/* 2 columns on desktop, 1 column on mobile */}
         <div className="mt-10 w-full flex-1 flex-col items-center justify-center md:w-3/4 md:flex-row">
-          <NewTeaParty />
+          {/* <NewTeaParty /> */}
+          <TeaPartyForm onSave={onSave} loading={loading} error={error} />
         </div>
       </div>
     </>
